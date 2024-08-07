8f23b40a83de8bed7a73ed4e9d99029ebd19d0f4
Rednaxelv <yuriescobara@gmail.com>
2024-08-07 13:22:30 -0500

    Add migration tables (ok) + model(not yet conf)

---
+++ database/migrations/2024_08_07_165647_create_payrolls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->string('tittle', 255)->nullable();
+            $table->string('year', 4);
+            $table->string('month', 50);
+            $table->string('file_uri', 255);
+            $table->foreignId('payroll_type_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payrolls');
+    }
+};
